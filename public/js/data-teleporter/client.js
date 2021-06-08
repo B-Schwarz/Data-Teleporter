47dc5178fe3e0d48f66c6837d74708ba260b6292
Ben Schwarz <29834059+B-Schwarz@users.noreply.github.com>
2021-06-08 16:42:40 +0200

    Added FileSaver + Server Shares File Names + Client Displays File Names

--- public/js/data-teleporter/client.js
+++ public/js/data-teleporter/client.js
@@ -1,16 +1,29 @@
 import {createNewPeer} from "./peer.js";
+
 let p = await createNewPeer("");
 
-async function a() {
-    console.log('Kalla')
-
+function connect() {
     const id = document.getElementById("id-text").value;
-    let conn = p.connect(id)
+    const ul = document.getElementById('id-ul');
+    const conn = p.connect(id)
 
+    console.log('lulu')
     conn.on('open', () => {
-        conn.send('Hello')
+        // const d = document.getElementById('id-file').files[0];
+        // conn.send(d)
+    })
+    conn.on('data', (data) => {
+        ul.innerHTML = '';
+        for (let f of data) {
+            let li = document.createElement("li")
+            li.appendChild(document.createTextNode(f))
+            ul.appendChild(li);
+        }
+    })
+    conn.on('close', () => {
+        document.getElementById('id-title').innerText = 'DISCONNECTED'
+        ul.innerHTML = '';
     })
 }
 
-console.log('Jello')
-window.a = a;
+window.connect = connect;
--- public/js/data-teleporter/server.js
+++ public/js/data-teleporter/server.js
@@ -1,13 +1,42 @@
 import {createNewPeer} from "./peer.js";
 
 let p = await createNewPeer("");
+const saveAs = window.saveAs;
+
+let files = document.getElementById('id-file').files
+
+function update_files() {
+    files = document.getElementById('id-file').files
+}
+
+function sendFileList(conn) {
+    let f = []
+
+    for (let i = 0; i < files.length; i++) {
+        f.push(files.item(i).name)
+    }
+
+    conn.send(f)
+}
 
 p.on('open', (id) => {
     console.log(id)
 })
 
 p.on('connection', (conn) => {
+    let interval;
+    conn.on('open', () => {
+        interval = setInterval(sendFileList, 200, conn)
+    })
     conn.on('data', (data) => {
-        console.log(data)
+        // let f = new File([data], "Affe");
+        // saveAs(f, "test.zip")
+    })
+    conn.on('close', () => {
+        if (interval) {
+            clearInterval(interval)
+        }
     })
 })
+
+window.update_files = update_files;
